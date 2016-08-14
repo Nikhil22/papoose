b8bba2d9f159a1ba87663f1fe076d7f06b280629
Nikhil Bhaskar <nikhil38@gmail.com>
2016-08-14 18:05:32 -0400

    initial commit

---
+++ gallop.js
@@ -0,0 +1,16 @@
+function gallop() {
+
+	var fns = arguments;
+
+  return function() {
+
+    var result = fns[0].apply(this, arguments);
+
+    for (var i = 1; i < fns.length; i++) {
+      result = fns[i](result);
+    }
+
+    return result;
+	};
+
+}
