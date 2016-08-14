92b53509fe80d427a31fc729523caa8bb6c168ed
Nikhil Bhaskar <nikhil38@gmail.com>
2016-08-14 18:24:21 -0400

    reformatted code

--- pony.js
+++ pony.js
@@ -5,12 +5,11 @@ function pipe() {
   return function() {
 
     var result = fns[0].apply(this, arguments);
-
     for (var i = 1; i < fns.length; i++) {
       result = fns[i](result);
     }
 
     return result;
 	};
-
+  
 }
